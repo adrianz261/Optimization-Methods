9974c0eb13585bee9b8cb7790217b830be9ac2fb
adrianz271 <adrian.zuchowski@outlook.com>
2021-10-25 17:23:08 +0200

    Fixed some bugs.

--- ExpansionMethod/expansionMethod.hpp
+++ ExpansionMethod/expansionMethod.hpp
@@ -14,14 +14,14 @@ using size = unsigned long int;
 
 bool isEqual(double a, double b, const double e = 0.001)
 {
-    return std::abs(a - b) <= e ? true: false;
+    return std::abs(a - b) <= e ? true : false;
 }
 
 std::pair<double, double> expansionMethod(double (*f)(const double x), double x1 = 1, const double a = 1.1, const size iterMax = 10000)
 {
-    std::array<double, 3> x = {0.0, x1, x1};
+    std::array<double, 3> x = {0.0, x1, 0.0};
     double y1 = f(x[1]);
-    const double y0 = f(0.0);
+    const double y0 = f(x[0]);
     
     
     if (isEqual(y1, y0))
@@ -32,7 +32,7 @@ std::pair<double, double> expansionMethod(double (*f)(const double x), double x1
     if (y1 > y0)
     {
         x[1] = -x[1];
-        x[2] = x[1];
+        //x[2] = x[1];
         y1 = f(x[1]);
     
         if (y1 >= y0)
@@ -41,18 +41,20 @@ std::pair<double, double> expansionMethod(double (*f)(const double x), double x1
         }
     }
 
-    for (size i = 1; i < iterMax; i++)
+    size i = 0;
+    do
     {
-        x[2] = a * x[1];
-
-        if (f(x[1]) <= f(x[2]))
+        if (i > iterMax)
         {
-            break;
+            return std::pair<double, double>();
         }
+        i += 1;
+
+        x[2] = a * x[1];
         x[0] = x[1];
         x[1] = x[2];
-        
-    }
+
+    } while (f(x[1]) > f(x[2]));
 
     if (x[0] < x[2])
     {
