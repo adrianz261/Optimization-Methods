77a5f387c043d77ff88e2ca032fbeb1c1a0f2542
adrianz261 <adrian.zuchowski@outlook.com>
2021-10-31 16:17:52 +0100

    Tested.

--- regulaFalsi/main.cpp
+++ regulaFalsi/main.cpp
@@ -6,16 +6,11 @@ double y(const double x)
     return x * x * x -1.2;
 }
 
-double y2(const double x)
-{
-    return x * x * x - x * x + 2;
-}
-
 int main()
 {
     double x0 = 0.0;
     
-    if (regulaFalsi(y2, x0, -200.0, 300.0))
+    if (regulaFalsi(y, x0, -200.0, 300.0))
     {
         std::cout << x0 << '\n';
     }
